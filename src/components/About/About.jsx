3c0877accd7fdf579e0a7f5c77b65f9ce4a4b8bf
Ritik Singh <ritik@example.com>
2025-11-07 20:12:32 +0530

    update cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1dk6ANZFndLFteYWV0snLAMrRCaplBQ96/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1gf8Y-fBkvUTSSCbfkzyUqBvAJj2JmRmb/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block dark:text-content-dark text-content-light py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
